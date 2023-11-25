c6b68ef4f27e2c1e1b12b258de3f1afed7aaa7da
Masl <marcel.z2006@gmail.com>
2023-11-25 18:34:38 +0100

    asdfasdfsdfds

--- rb-taktik/pages/home/home.js
+++ rb-taktik/pages/home/home.js
@@ -2,3 +2,25 @@ function joinGame()
 {
     goPage("/ingame")
 }
+
+function openOptions()
+{
+    alert("<OPTIONS GO HERE>");
+}
+
+function joinRankedGame()
+{
+    joinGame();
+}
+
+function joinRandomGame()
+{
+    joinGame();
+}
+
+
+function createGame()
+{
+    joinGame();
+}
+
