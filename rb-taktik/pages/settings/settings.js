de2a5f35865ef5c067be90ea53290ae9b9f578e1
diekenz06 <200138@studierende.htl-donaustadt.at>
2023-12-09 21:28:37 +0100

    update settings

--- rb-taktik/pages/settings/settings.js
+++ rb-taktik/pages/settings/settings.js
@@ -1,10 +1,3 @@
-import { createRoot } from '../node_modules/react-dom/client';
-
-function NavigationBar() {
-  // TODO: Actually implement a navigation bar
-  return <h1>Hello from React!</h1>;
+function showValue(x, output){
+    output.innerText= x;
 }
-
-const domNode = document.getElementById('slider');
-const root = createRoot(domNode);
-root.render(<NavigationBar />);
