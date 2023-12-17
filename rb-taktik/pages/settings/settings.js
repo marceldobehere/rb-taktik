6438fac690eb88f77bb850f88d045fc2d78755b6
diekenz06 <200138@studierende.htl-donaustadt.at>
2023-12-18 00:53:15 +0100

    late update settings page 18.12.2023

--- rb-taktik/pages/settings/settings.js
+++ rb-taktik/pages/settings/settings.js
@@ -1,3 +1,19 @@
+let savedValue;
+
 function showValue(x, output){
-    output.innerText= x + "%";
+    savedValue = x;
+    output.innerText= savedValue + "%";
 }
+
+function updateValue(x, output){
+    savedValue = x;
+    output.innerText= savedValue + "%";
+}
+
+// window.addEventListener("load", myInit, true); 
+
+// function myInit(){  
+
+//     showValue(document.getElementById("volumeMusicValue").value, document.getElementById("volumeMusic"));
+//     showValue(document.getElementById("volumeFXValue").value, document.getElementById("volumeFX"));
+// }; 
