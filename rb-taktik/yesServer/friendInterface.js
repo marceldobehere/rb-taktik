0733d723ea99fb05802cdd9b083dc3d47a519383
Masl <70035395+marceldobehere@users.noreply.github.com>
2024-01-30 12:59:08 +0100

    more work on friend interface

--- rb-taktik/yesServer/friendInterface.js
+++ rb-taktik/yesServer/friendInterface.js
@@ -18,17 +18,61 @@ async function initApp(_dbInterface, _notificationInterface, _accountInterface, 
 
 async function getAllFriendsForUser(userId)
 {
-    return [];
+    let friendUser = await dbInterface.getPair("notifications", userId);
+    if (friendUser === undefined)
+        return [];
+
+    return friendUser.friends;
 }
 
 async function areFriends(userId1, userId2)
 {
-    return false;
+    let friendUser1 = await dbInterface.getPair("notifications", userId1);
+    if (friendUser1 === undefined)
+        return false;
+
+    let friendUser2 = await dbInterface.getPair("notifications", userId2);
+    if (friendUser2 === undefined)
+        return false;
+
+    return friendUser1.friends.includes(userId2) && friendUser2.friends.includes(userId1);
+}
+
+async function getUserThing(userId)
+{
+    if (await _accountInterface.getUser(userId) == undefined)
+        return undefined;
+
+    let friendUser = await dbInterface.getPair("notifications", userId);
+    if (friendUser === undefined)
+    {
+        friendUser = {friends: []};
+        await dbInterface.addPair("friends", userId, friendUser);
+    }
+
+    return friendUser;
 }
 
 async function addFriend(userId1, userId2)
 {
-    return false;
+    let friendUser1 = getUserThing(userId1);
+    if (friendUser1 === undefined)
+        return false;
+
+    let friendUser2 = getUserThing(userId2);
+    if (friendUser2 === undefined)
+        return false;
+
+    if (await areFriends(userId1, userId2))
+        return false;
+
+    friendUser1.friends.push(userId2);
+    await dbInterface.updatePair("friends", userId1, friendUser1);
+
+    friendUser2.friends.push(userId1);
+    await dbInterface.updatePair("friends", userId2, friendUser2);
+
+    return true;
 }
 
 async function removeFriend(userId1, userId2)
