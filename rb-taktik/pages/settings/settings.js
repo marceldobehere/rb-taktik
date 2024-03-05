8ae336e1e037d9dfdcd8540ac082623f6fef0fac
diekenz06 <200138@studierende.htl-donaustadt.at>
2024-03-05 12:03:46 +0100

    a

--- rb-taktik/pages/settings/settings.js
+++ rb-taktik/pages/settings/settings.js
@@ -19,3 +19,40 @@ onModulesImported.push(() => {
 });
 
 startBgm(bgmMenuAudio);
+
+const btns = document.querySelectorAll(".sound");
+
+btns.forEach(function(btn) {
+  let hoverSoundPlayed = false;
+
+  btn.addEventListener("click", function() {
+    fxAudioMenuButtonClick.play()
+      .then(() => {
+        console.log("fx played");
+      })
+      .catch(() => {
+        // console.error("error");
+      });
+  });
+
+  btn.addEventListener("mouseover", function() {
+    if (!hoverSoundPlayed) {
+      fxAudioMenuButtonHover.volume = 0.25;
+      fxAudioMenuButtonHover.play()
+        .then(() => {
+          console.log("hover sound played");
+        })
+        .catch(() => {
+        //   console.error("error");
+        });
+      hoverSoundPlayed = true;
+    }
+  });
+
+  btn.addEventListener("mouseout", function(event) {
+    const relatedTarget = event.relatedTarget;
+    if (!relatedTarget || (relatedTarget !== btn && !btn.contains(relatedTarget))) {
+      hoverSoundPlayed = false;
+    }
+  });
+});
